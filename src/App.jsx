d4be9a3a946dc224aa8f0a020bc74ddc30d5fbe8
khalidhamdi <khalidhamdi204@gmail.com>
2024-09-21 17:37:51 +0300

    feat: implement property addition through 'Airbnb Your Home' and initialize category page

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,14 @@
 import React from 'react';
 import { Routes, Route } from 'react-router-dom';
 import Home from './pages/home/Home';
+import CategoryPage from './pages/category/CategoryPage';
 
 function App() {
   return (
     <Routes>
       <Route path="/home" element={<Home />} />
       <Route path="/" element={<Home />} />
+      <Route path="/category/:slug" element={<CategoryPage />} />
     </Routes>
   );
 }
--- src/main.jsx
+++ src/main.jsx
@@ -6,6 +6,8 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import 'bootstrap-icons/font/bootstrap-icons.css';
 import 'bootstrap/dist/js/bootstrap.bundle.min.js';
 import '@fortawesome/fontawesome-free/css/all.min.css';
+import store from './redux/store';
+import { Provider } from 'react-redux';
 
 
 
@@ -13,7 +15,9 @@ import '@fortawesome/fontawesome-free/css/all.min.css';
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <BrowserRouter>
-      <App />
-    </BrowserRouter>
+    <Provider store={store}>
+        <App />
+    </Provider>
+        </BrowserRouter>
   </React.StrictMode>
 );
