c2993b3b966026dbd29d7d561eade6a5fd7ac333
IBRAHIM SABER <ibrahimsaber622@gmail.com>
2024-09-26 15:02:27 +0300

    edit: SignupModal

--- src/components/modals/SignupModal.jsx
+++ src/components/modals/SignupModal.jsx
@@ -1,33 +1,39 @@
-import React, { useState } from "react";
-import { useNavigate } from "react-router-dom";
-import Modal from "./Modal";
-import useSignupModal from "../../hooks/useSignupModal";
-import CustomButton from "../forms/CustomButton.jsx";
-import apiService from "../../services/apiService";
-import { handleLogin } from "../../lib/actions";
+// src/components/modals/SignupModal.jsx
+import React, { useState } from 'react';
+import { useNavigate } from 'react-router-dom';
+import Modal from './Modal';
+import { useSelector, useDispatch } from 'react-redux';
+import { closeSignupModal } from '../../redux/modalSlice';
+import CustomButton from '../forms/CustomButton';
+import { handleLogin } from '../../lib/actions';
+import apiService from '../../services/apiService';
 
 const SignupModal = () => {
   const navigate = useNavigate();
-  const signupModal = useSignupModal();
+  const dispatch = useDispatch();
+  const isOpen = useSelector((state) => state.modal.signupModalOpen);
   const [email, setEmail] = useState('');
   const [password1, setPassword1] = useState('');
   const [password2, setPassword2] = useState('');
-  const [errors, setErrors] = useState([]);
+
+  const close = () => {
+    dispatch(closeSignupModal());
+  };
 
   const submitSignup = async (e) => {
     e.preventDefault();
 
     const formData = {
-      email: email,
-      password1: password1,
-      password2: password2
+      email,
+      password1,
+      password2,
     };
 
     const response = await apiService.postWithoutToken('/api/auth/register/', formData);
 
     if (response.access) {
       handleLogin(response.user.pk, response.access, response.refresh);
-      signupModal.close();
+      close();
       navigate('/');
     } else {
       const tmpErrors = Object.values(response).flat();
@@ -88,14 +94,8 @@ const SignupModal = () => {
     </form>
   );
 
-  return (
-    <Modal
-      isOpen={signupModal.isOpen}
-      close={signupModal.close}
-      label="Sign up"
-      content={content}
-    />
-  );
+  return <Modal isOpen={isOpen} close={close} label="Sign up" content={content} />;
+
 };
 
 export default SignupModal;
