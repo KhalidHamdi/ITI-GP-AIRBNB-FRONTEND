aefb66e07a0f4551d82590a61b30dd3b0a90e7e3
IBRAHIM SABER <ibrahimsaber622@gmail.com>
2024-09-26 15:03:55 +0300

    remove : hooks cause no longer using Zustand,

--- src/hooks/useLoginModal.js
+++
@@ -1,9 +0,0 @@
-import { create } from "zustand";
-
-const useLoginModal = create((set) => ({
-  isOpen: false,
-  open: () => set({ isOpen: true }),
-  close: () => set({ isOpen: false }),
-}));
-
-export default useLoginModal;
--- src/hooks/useSignupModal.js
+++
@@ -1,9 +0,0 @@
-import { create } from "zustand";
-
-const useSignupModal = create((set) => ({
-  isOpen: false,
-  open: () => set({ isOpen: true }),
-  close: () => set({ isOpen: false }),
-}));
-
-export default useSignupModal;
