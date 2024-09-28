a1bb3448986a9db97ecc45ff2861c386f0ea902a
IBRAHIM SABER <ibrahimsaber622@gmail.com>
2024-09-28 14:46:20 +0300

    fix: chat files

--- src/axios.tsx
+++ src/axios.tsx
@@ -3,63 +3,11 @@ import axios from 'axios';
 
 const axiosInstance = axios.create({
     baseURL: 'http://localhost:8000', 
-    withCredentials: true,
-    // headers: {
-    //   'Content-Type': 'multipart/form-data',
-    // },
-    // withCredentials: true, 
+    // withCredentials: true, // Set to true if your backend requires credentials like cookies
+    headers: {
+      'Content-Type': 'multipart/form-data',
+    },
+    withCredentials: true, 
 });
 
 export default axiosInstance;
-// import axios from 'axios';
-
-// const axiosInstance = axios.create({
-//     baseURL: 'http://localhost:8000',
-//     headers: {
-//       'Content-Type': 'multipart/form-data',
-//     },
-// });
-
-// axiosInstance.interceptors.request.use(config => {
-//   const token = localStorage.getItem('accessToken');  // Get accessToken from localStorage
-//   if (token) {
-//     config.headers['Authorization'] = `Bearer ${token}`; // Add token to headers
-//   }
-//   return config;
-// }, error => {
-//   return Promise.reject(error);
-// });
-
-// export default axiosInstance;
-
-// axiosInstance.interceptors.response.use(
-//   response => response,
-//   async error => {
-//     const originalRequest = error.config;
-    
-//     if (error.response.status === 401 && !originalRequest._retry) { 
-//       originalRequest._retry = true;
-
-//       const refreshToken = localStorage.getItem('refreshToken');  
-      
-//       if (refreshToken) {
-//         try {
-//           const response = await axios.post('http://localhost:8000/api/auth/refresh-token/', { refreshToken });
-//           const { accessToken } = response.data;
-          
-//           localStorage.setItem('accessToken', accessToken);  
-
-//           axiosInstance.defaults.headers.common['Authorization'] = `Bearer ${accessToken}`;
-//           originalRequest.headers['Authorization'] = `Bearer ${accessToken}`;
-
-//           return axiosInstance(originalRequest);
-//         } catch (err) {
-//           console.error("Failed to refresh token", err);
-//         }
-//       }
-//     }
-
-//     return Promise.reject(error);
-//   }
-// );
-
