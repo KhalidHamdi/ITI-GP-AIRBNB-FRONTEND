19e883939cdb362cc08e5cd8d80a9396704057a8
IBRAHIM SABER <ibrahimsaber622@gmail.com>
2024-09-28 11:34:33 +0300

    remove : 'Content-Type': 'multipart/form-data' from axios.js

--- src/axios.tsx
+++ src/axios.tsx
@@ -3,10 +3,11 @@ import axios from 'axios';
 
 const axiosInstance = axios.create({
     baseURL: 'http://localhost:8000', 
-    headers: {
-      'Content-Type': 'multipart/form-data',
-    },
-    withCredentials: true, 
+    withCredentials: true,
+    // headers: {
+    //   'Content-Type': 'multipart/form-data',
+    // },
+    // withCredentials: true, 
 });
 
 export default axiosInstance;
