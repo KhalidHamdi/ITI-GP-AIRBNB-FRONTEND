3c3150042cac0141160be878c5daf9a72084995d
David Emad <demad2527@gmail.com>
2024-10-01 15:37:56 +0300

    fix name of the chat pug

--- src/components/chat/conversation.jsx
+++ src/components/chat/conversation.jsx
@@ -3,6 +3,8 @@ import { Link } from "react-router-dom";
 
 const Conversation = ({ conversation, currentUserId }) => {
   console.log("conversation", conversation);
+
+  // Check if the current user is part of the conversation
   const isCurrentUserPartOfConversation = conversation.users.some(
     (user) => user.id === currentUserId
   );
@@ -11,13 +13,15 @@ const Conversation = ({ conversation, currentUserId }) => {
     return null;
   }
 
+  // Determine sender and receiver based on the current user's ID
+  const sender = conversation.users.find((user) => user.id === currentUserId);
+  const receiver = conversation.users.find((user) => user.id !== currentUserId);
+
   return (
     <div className="conversation-container">
       <div className="conversation" key={conversation.id}>
         <div className="conversationBox">
-          <h3 className="conversation-title">
-            {conversation.users[0].username}{" "}
-          </h3>
+          <h3 className="conversation-title">{receiver.username}</h3>
           <p className="conversation-link">
             <Link to={`/conversationDetail/${conversation.id}`}>
               Go to conversation
