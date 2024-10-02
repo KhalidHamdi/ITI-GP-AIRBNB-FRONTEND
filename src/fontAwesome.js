43984f087b5ae1e08a08e3c37376a5a54e2573cc
IBRAHIM SABER <ibrahimsaber622@gmail.com>
2024-10-02 14:18:43 +0300

    edit the style of reviews

---
+++ src/fontAwesome.js
@@ -0,0 +1,8 @@
+// src/fontAwesome.js
+
+import { library } from '@fortawesome/fontawesome-svg-core';
+import { faEye, faEyeSlash } from '@fortawesome/free-solid-svg-icons';
+import { faStar as solidStar } from '@fortawesome/free-solid-svg-icons';
+import { faStar as regularStar } from '@fortawesome/free-regular-svg-icons';
+
+library.add(faEye, faEyeSlash,solidStar, regularStar);
