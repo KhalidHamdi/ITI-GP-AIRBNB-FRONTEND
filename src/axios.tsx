749280fe09779ef5f39289d86f5eb0ace3908e5a
David Emad <demad2527@gmail.com>
2024-10-03 06:09:41 +0300

    test deploy

--- src/axios.tsx
+++ src/axios.tsx
@@ -2,7 +2,7 @@ import axios from "axios";
 import Cookies from "js-cookie";
 
 const axiosInstance = axios.create({
-  baseURL: "https://itnb.up.railway.app/",
+  baseURL: "http://localhost:8000/",
   headers: {
     "Content-Type": "multipart/form-data",
   },
