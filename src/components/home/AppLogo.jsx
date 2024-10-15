53223c830e6a72b0e67e9e616a685d27099de84a
khalidhamdi <khalidhamdi204@gmail.com>
2024-10-15 08:38:32 +0300

    fix app logo issue

--- src/components/home/AppLogo.jsx
+++ src/components/home/AppLogo.jsx
@@ -1,13 +1,13 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import logo from '../../assets/ITI-bnb.png'; 
 
 const AppLogo = () => {
   return (
     <Link to="/" className="navbar-brand">
-      <img src="../src/assets/ITI-bnb.png" alt="Airbnb" height="60" />
+      <img src={logo} alt="Airbnb" height="60" />
     </Link>
   );
 };
 
 export default AppLogo;
-
